7c2471e89e04ff5fc0de2ce71364a5af1c862a8a
Tonya <rickmay@ya.ru>
2020-03-12 15:17:16 +0300

    12.03

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -75,6 +75,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:category_id, :type, :name, :description)
+      params.require(:product).permit(:category_id, :type_id, :name, :ingridient_ids, :description)
     end
 end
