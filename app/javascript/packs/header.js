580be2d4a70a7c09013b6e370114a998b5cad1d1
Tonya <rickmay@ya.ru>
2020-04-27 07:10:37 +0300

    Контент настроен, кроме хэдера в лендосе

---
+++ app/javascript/packs/header.js
@@ -0,0 +1,11 @@
+var scrolled;
+window.onscroll = function() {
+    scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrolled > 1){
+        $("menu").css({"border-bottom": "0.5px solid black"})
+    }
+    if(100 > scrolled){
+        $("menu").css({"border-bottom": "none"})
+    }
+
+}
