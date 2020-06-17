85e0ff09c4ccb033fa01f1b4f78c66c63a3bebc8
Antonina Vetlugaeva <rickmay@ya.ru>
2020-06-17 06:59:34 +0300

    Отзывы кайфовые, но нет авы

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def set_favourite
-    @favourite = User.fiest.favourites.last
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :password, presence: true
   validates :email, presence: true
+  validates :username, presence: true
 end
