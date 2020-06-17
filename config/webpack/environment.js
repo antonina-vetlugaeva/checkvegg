da742c575903e1b24a3e653b5cdab5f744b7c797
Antonina Vetlugaeva <rickmay@ya.ru>
2020-06-17 09:42:52 +0300

    рейтинг при написании отзыва

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,3 +5,11 @@ const vue = require('./loaders/vue')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
